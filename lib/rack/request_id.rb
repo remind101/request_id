091c2e7a484c76a84bb88be9146a953ecfecdb6f
Eric J. Holmes <eric@ejholmes.net>
2013-09-12 09:46:49 -0700

    Cleanup.

--- lib/rack/request_id.rb
+++ lib/rack/request_id.rb
@@ -6,7 +6,7 @@ module Rack
   # Heroku has a labs feature called request_id, which can be used to tracking
   # a request through the system.
   #
-  # app     - The Rack app.
+  # app - The Rack app.
   #
   # Examples
   #
@@ -14,7 +14,6 @@ module Rack
   #
   #   logger.info "request_id=#{Thread.current[:request_id]} Hello world"
   #   # => request_id=a08a6712229fb991c0e5026c246862c7 Hello world
-  #
   class RequestId
     REQUEST_ID_HEADER = 'HTTP_HEROKU_REQUEST_ID'.freeze
 
